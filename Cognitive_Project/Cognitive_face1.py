29abef2361524df4ed8805868c37b0cad47ad359
sysnick <nick.hou@outlook.com>
2017-12-28 14:55:50 +0800

    update20171228

--- Cognitive_Project/Cognitive_face1.py
+++ Cognitive_Project/Cognitive_face1.py
@@ -3,9 +3,15 @@ KEY = 'b7f92957bb354f96b6907df47895db2d'
 
 CF.Key.set(KEY)
 
+CF.util.Key.get()
+
+print(CF.util.Key.get())
+
 Base_URL = 'https://eastasia.api.cognitive.microsoft.com/face/v1.0'
 
 CF.BaseUrl.set(Base_URL)
+
+print(CF.BaseUrl.get())
 
 img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
 
